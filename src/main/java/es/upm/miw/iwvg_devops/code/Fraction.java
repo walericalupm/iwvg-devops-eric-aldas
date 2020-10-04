d3e86af979dd9123dc0cf892d84b19176abdf8b6
Walter Aldas <walerical18@hotmail.com>
2020-10-04 12:33:35 +0200

    Fraction class created

---
+++ src/main/java/es/upm/miw/iwvg_devops/code/Fraction.java
@@ -0,0 +1,50 @@
+package es.upm.miw.iwvg_devops.code;
+
+/***
+ * Author: Eric Aldas
+ * Based in: miw-upm/iwvg-devops project
+ *
+ */
+public class Fraction {
+
+    private int numerator;
+
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public Fraction() {
+        this(1, 1);
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public void setDenominator(int denominator) {
+        this.denominator = denominator;
+    }
+
+    public double decimal() {
+        return (double) numerator / denominator;
+    }
+
+    @Override
+    public String toString() {
+        return "Fraction{" +
+                "numerator=" + numerator +
+                ", denominator=" + denominator +
+                '}';
+    }
+}
