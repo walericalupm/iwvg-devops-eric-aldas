206730671947dc10744d03c299d50dff684b3cdf
Walter Aldas <walerical18@hotmail.com>
2020-10-04 18:43:48 +0200

    Fraction test created #12

--- src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
@@ -1,7 +1,44 @@
 package es.upm.miw.iwvg_devops.code;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FractionTest {
 
+    private Fraction fractionToTest;
+
+    @BeforeEach
+    void before() {
+        this.fractionToTest = new Fraction(1,2);
+    }
+
+    @Test
+    void getNumerator() {
+        assertEquals(1, this.fractionToTest.getNumerator());
+    }
+
+    @Test
+    void setNumerator() {
+        this.fractionToTest.setNumerator(3);
+        assertEquals(3, this.fractionToTest.getNumerator());
+    }
+
+    @Test
+    void getDenominator() {
+        assertEquals(2, this.fractionToTest.getDenominator());
+    }
+
+
+    @Test
+    void setDenominator() {
+        this.fractionToTest.setDenominator(5);
+        assertEquals(5, this.fractionToTest.getDenominator());
+    }
+
+    @Test
+    void decimal() {
+        assertEquals(0.5, this.fractionToTest.decimal());
+    }
 }
